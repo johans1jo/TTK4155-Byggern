1b9ef9e4d77614881a1c63c2797b129e17521923
Erlend Blomseth <erlend@elendig.no>
2019-09-04 17:13:23 +0200

    SRAM funker, god S

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include "uart.h"
+#include "sram.h"
+#include <avr/delay.h>
 
 #define FOSC 4915200 // Clock Speed
 #define BAUD 9600
@@ -8,18 +10,8 @@
 
 void main(void) {
   uart_init(MYUBRR);
-  printf("Ting funker");
-  /*
-
-  // uart_transmit- og uart_receive-kallene under funker ikke
-  // pga endrede funksjonsdeklarasjoner
-
-  while (1) {
-    if (uart_receive()) {
-      unsigned char hei = uart_receive();
-      uart_transmit ('o');
-      uart_transmit ('k');
-      uart_transmit (hei);
-    }
-  }*/
+  SRAM_init();
+//  _delay_ms(3000);
+  //SRAM_test();
+  SRAM_test();
 }
