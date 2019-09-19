ea4c4360ad9513db618b375ec20b774b71f93d9a
Erlend Blomseth <erlend@elendig.no>
2019-09-19 17:44:07 +0200

    spi antakelig ok

--- spi.c
+++ spi.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include "uart.h"
 #include <avr/interrupt.h>
+#include "MCP2515.h"
 
 #define DDR_SPI DDRB
 #define DD_SS PB4
@@ -9,17 +10,23 @@
 #define DD_MISO PB6
 #define DD_SCK PB7
 
-void spi_master_init(void) {
-	/* Set MOSI and SCK output, all others input */
-	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK);
+void spi_master_init() {
+	/* Set MOSI and SCK - og SS - output, all others input */
+	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK)|(1<<DD_SS);
 	/* Enable SPI, Master, set clock rate fck/16 */
-	DDRB = (1 << PB4);
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
-	printf("\r\nMSTR etter master init: %x", SPCR & (1 << MSTR));
 
+	spi_set_ss();
 }
 
-void spi_master_transmit(char cData) {
+void spi_slave_init() {
+	/* Set MISO output, all others input */
+	DDR_SPI = (1<<DD_MISO);
+	/* Enable SPI */
+	SPCR = (1<<SPE);
+}
+
+void spi_write(char cData) {
 	/* Start transmission */
 	SPDR = cData;
 	/* Wait for transmission complete */
@@ -27,17 +34,19 @@ void spi_master_transmit(char cData) {
 	}
 }
 
-void spi_slave_init(void) {
-	/* Set MISO output, all others input */
-	DDR_SPI = (1<<DD_MISO);
-	/* Enable SPI */
-	SPCR = (1<<SPE);
-}
-
-char spi_slave_receive(void) {
+char spi_read() {
+	//SPDR = 0 ?
 	/* Wait for reception complete */
 	while(!(SPSR & (1<<SPIF))) {
 	}
 	/* Return data register */
 	return SPDR;
+}
+
+void spi_set_ss() {
+	PORTB = (1 << DD_SS);
+}
+
+void spi_clear_ss() {
+	PORTB = (0 << DD_SS);
 }
--- spi.h
+++ spi.h
@@ -1,4 +1,6 @@
-void spi_master_init(void);
-void spi_master_transmit(char cData);
-void spi_slave_init(void);
-char spi_slave_receive(void);
+void spi_master_init();
+void spi_slave_init();
+void spi_write(char cData);
+char spi_read();
+void spi_set_ss();
+void spi_clear_ss();
