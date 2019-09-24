89e6a4dcee3f866476b09d96674e51dee3799634
Erlend Blomseth <erlend@elendig.no>
2019-09-24 19:00:29 +0200

    draw funker litt

--- draw.c
+++ draw.c
@@ -1,25 +1,48 @@
 #include "draw.h"
+#include "sram.h"
+#include "oled.h"
 #include <stdio.h>
 
-unsigned char pixels[128][8] = { 0 };
+void draw_init() {
+	draw_clear();
+}
 
-
-// Tegner linje fra punktet (x1, y1) til punktet (x2, y2)
+// Tegner linje fra punktet (x1, y1) til punktet (x2, y2) med størrelse size
 void draw_line(int x1, int y1, int x2, int y2, int size) {
-	for (int x = x1; x < x2; x++) {
-		int s = (y2 - y1)*1000 / (x2 - x1);
-		int y = y1 + (s*(x - x1)) / 1000;
-		printf(".%d", y);
-		draw_point(x, y, size);
+	if (x1 > x2) {
+		int tmp = x1;
+		x1 = x2;
+		x2 = tmp;
+	}
+	if (y1 > y2) {
+		int tmp = y1;
+		y1 = y2;
+		y2 = tmp;
+	}
+		for (int x = x1; x < x2; x++) {
+			int s = (y2 - y1)*1000 / (x2 - x1);
+			int y = y1 + (s*(x - x1)) / 1000;
+			draw_point(x, y, size);
+		}
+}
+
+void draw_lines(point * points, int length, int size) {
+	for (int i = 0; i < length - 1; i++) {
+		printf("x %d y %d\r\n", points[i+1].x, points[i+1].y);
+		draw_line(points[i].x, points[i].y, points[i+1].x, points[i+1].y, size);
 	}
 }
 
-// Tegn et punkt med gitt tykkelse
+void draw_circle(int x1, int y1, int width, int size) {
+	//(x−h)2+(y−k)2=r2
+}
+
+// Tegner et punkt på posisjon (x1, y1) med størrelse size
 void draw_point(int x1, int y1, int size) {
 	if (size == 0) {
 		return;
 	}
-	set_bit(x1, y1);
+	draw_set_bit(x1, y1);
 
 	if (size > 1) {
 
@@ -38,12 +61,46 @@ void draw_point(int x1, int y1, int size) {
 	}
 }
 
-void set_bit(int x, int y) {
+void draw_set_bit(int x, int y) {
 	int line = y/8;
 	int bit = y%8;
-	pixels[x][line] |= (1 << bit);
+	//pixels[x][line] |= (1 << bit);
+
+	int address = x*8 + line;
+	char byte = sram_read(address);
+
+	byte |= (1 << bit);
+
+	sram_write(address, byte);
+	char byte2 = sram_read(address);
 }
 
-void print_pixels() {
-	//oled
+void draw_clear_bit(int x, int y) {
+	int line = y/8;
+	int bit = y%8;
+
+	int address = x*8 + line;
+	char byte = sram_read(address);
+
+	byte &= ~(1 << bit);
+
+	sram_write(address, byte);
+	char byte2 = sram_read(address);
+}
+
+void draw_clear() {
+	for (int address = 0; address < 128*8; address++) {
+		sram_write(address, 0);
+	}
+}
+
+void draw_print() {
+	for (int x = 0; x < 128; x++) {
+		for (int line = 0; line < 8; line++) {
+			int address = x*8 + line;
+			char byte = sram_read(address);
+			oled_goto_pos(line, x);
+			oled_write_data(byte);
+		}
+	}
 }
--- draw.h
+++ draw.h
@@ -1,10 +1,22 @@
 #ifndef HEI
 #define HEI
-unsigned char pixels[128][8];
-void set_bit(int x, int y);
-void print_pixels();
+
+// Linje
+typedef struct Point {
+	int x;
+	int y;
+} point;
+
+
+void draw_set_bit(int x, int y);
+void draw_clear_bit(int x, int y);
+void draw_print();
+void draw_init();
+void draw_clear();
 
 void draw_point(int x1, int y1, int size);
+void draw_circle(int x1, int y1, int width, int size);
 void draw_line(int x1, int y1, int x2, int y2, int size);
+void draw_lines(point * points, int length, int size);
 
 #endif
