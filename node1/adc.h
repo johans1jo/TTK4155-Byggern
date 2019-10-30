83119d141e98fdd4f6a897b9c0f68aef34d77acb
Max Robertson <maxmr@stud.ntnu.no>
2019-10-30 16:40:38 +0100

    hei

--- node1/adc.h
+++ node1/adc.h
@@ -1,3 +1,8 @@
+/**
+*@file
+*@brief A driver for the adc containing functions to initialize the adc and read the data from the joystick
+*/
+
 #include <avr/io.h>
 
 enum {
@@ -7,5 +12,16 @@ enum {
     SLIDER_RIGHT
 };
 
+/**
+ @brief Sets the SRE-bit in the MCU Control Register (MCUCR) to enable External Memory Interface. Releases Port C Pin 7-4 for normal Port Pin function.
+ */
 void adc_init();
+
+/**
+ @brief Allocates the adress space of the ADC to begin at base address Ox1400 in the External Memory space. Creates a pointer to the base address, and reads the value from the memory address.
+ 
+ @param[in] channel The channel from the ADC
+ 
+ @return The value read from the joystick 
+ */
 int adc_read(int channel);
