8826235ec16d7e74b9fb031184149a667baa6dcf
Max Robertson <maxmr@stud.ntnu.no>
2019-11-06 15:25:46 +0100

    tjohei

--- node1/can.h
+++ node1/can.h
@@ -12,17 +12,26 @@ typedef struct Message {
 /**
  @brief Initializes the CAN by calling the function mcp_init
 
- 
+
  */
 void can_init();
 
 /**
- @brief Sends a message to the nodes over CAN. Sets the message-id in the TXB0SIDH
- and TXB0SIDL registers of the MCP2515, sets the number of data bytes to be transmitted in the
- TXB0DLC register, and writes the registers beginning at TXB0D0.
+ @brief Sends a message to the nodes over CAN. Sets the 11-bit message-id by setting the 8 higher bits in the TXB0SIDH register
+ and the 3 lower bits in the TXB0SIDL register of the MCP2515, sets the number of data bytes to be transmitted in the
+ TXB0DLC register, and writes data bytes to the TXBnDm register(s).
 
- @param[in] message Takes a pointer to the Message-struct we would like to send
+ @param[in] message Takes a pointer to the Message-struct we would like to send.
  */
 void can_send(message_ptr message);
+
+
+/**
+ @brief Reads a message sent over CAN. Assembles the id of the received message by reading the 8 higher bits in the RXB0SIDL register
+ and the 3 lower bits in the RXB0SIDH register, reads the number of data bytes transmitted by reading RXB0DLC, and reads the data bytes
+ from the RXBnDm register(s).
+
+ @return The data byte(s) of the received message from the RXBnDm register(s).
+ */
 message_t can_receive();
 void can_interrupt();
