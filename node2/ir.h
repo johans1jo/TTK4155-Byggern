bf89304ff15eeabade709f500db7e8e2038b4624
Max Robertson <maxmr@stud.ntnu.no>
2019-11-19 11:37:46 +0100

    dette var det jeg rakk

--- node2/ir.h
+++ node2/ir.h
@@ -1,2 +1,17 @@
+/**
+ * @file
+ * @brief Implementation of the IR.
+ */
+
+ /**
+  * @brief Initializes the IR.
+  *
+  */
 void ir_init();
+
+/**
+ * @brief Reads the value from the IR.
+ *
+ * @return The value from the IR.
+ */
 int ir_read();
