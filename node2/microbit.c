af7905469663a004b68db7df56babd891d360509
Erlend Blomseth <erlend@elendig.no>
2019-11-19 09:53:38 +0100

    microbit-uart fix

--- node2/microbit.c
+++ node2/microbit.c
@@ -1,16 +1,16 @@
 #include "microbit.h"
 
 void microbit_init(unsigned int ubrr) {
-	UBRR0H = 0;
-	UBRR0L = 0;
+	UBRR2H = 0;
+	UBRR2L = 0;
 
-	UCSR0C = (1<<USBS0); // 2 stoppbit
-	UCSR0C = (3<<UCSZ00); // 8 bits per character
+	UCSR2C &= ~(1<<USBS2); // 2 stoppbit
+	UCSR2C |= (3<<UCSZ20); // 8 bits per character
 
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // Enable receiver and transmitter
+	UCSR2B = (1<<RXEN2)|(1<<TXEN2); // Enable receiver and transmitter
 
-	UBRR0H = (ubrr >> 8); // 0
-	UBRR0L = ubrr; // 103
+	UBRR2H = (ubrr >> 8); // 0
+	UBRR2L = ubrr; // 103
 
 	//fdevopen(uart_transmit, uart_receive); // fra uart.c
 }
@@ -20,15 +20,15 @@ void microbit_init(unsigned int ubrr) {
 //unsigned char?
 void microbit_transmit(char data) {
 	// Wait for empty transmit buffer
-	while (! (UCSR0A & (1 << UDRE0)) );
+	while (! (UCSR2A & (1 << UDRE2)) );
 	// Put data into buffer, sends the data
-	UDR0 = data;
+	UDR2 = data;
 }
 
 // int uart_receive(FILE * file){ // fra uart.c
 unsigned char microbit_receive() {
 	// Wait for data to be received
-	while ( !(UCSR0A & (1 << RXC0)) );
+	while ( !(UCSR2A & (1 << RXC2)) );
 	// Get and return received data from buffer
-	return UDR0;
+	return UDR2;
 }
