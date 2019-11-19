c1624630da007c48c7fde7709991b47832c0a3a6
Max Robertson <maxmr@stud.ntnu.no>
2019-11-19 17:20:49 +0100

    ferdig

--- node2/solenoid.h
+++ node2/solenoid.h
@@ -1,5 +1,31 @@
+/**
+ * @file
+ * @brief A driver for the solenoid.
+ */
+
+ /**
+  * @brief Initializes the solenoid.
+  */
 void solenoid_init();
+
+/**
+ * @brief Sets the solenoid.
+ */
 void solenoid_set();
+
+/**
+ * @brief Clears the solenoid.
+ */
 void solenoid_clear();
+
+/**
+ * @brief Checks if the solenoid is set.
+ *
+ * @return A boolean corresponding to if the solenoid is set or not.
+ */
 int solenoid_is_set();
+
+/**
+ * @brief Fires the solenoid.
+ */
 void solenoid_fire();
